3e0acd9e69bd9b6c4ff2f39ad7f0eec49032ecfd
Drew Collins <drewcoll@uw.edu>
2014-08-15 13:02:02 -0700

    Removing timeout error if statement

--- page.php
+++ page.php
@@ -9,18 +9,15 @@
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>" class="template-page">
 			<?php do_action('foundationPress_page_before_entry_content'); ?>
 			<div class="entry-content">
-					<?php while ( have_posts() ) : the_post() ?>
 
 						<?php get_template_part( 'partials/partial', 'article' ) ?>
 
-					<?php endwhile ?>
 			</div>
 			<footer>
 				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
 				<p><?php the_tags(); ?></p>
 			</footer>
 			<?php do_action('foundationPress_page_before_comments'); ?>
-			<?php comments_template(); ?>
 			<?php do_action('foundationPress_page_after_comments'); ?>
 		</article>
 	<?php endwhile;?>
