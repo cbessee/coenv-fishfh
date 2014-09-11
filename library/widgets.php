b35e2f8c9bfdc07c14d04772fa4e370104603718
Joe Eastham <jeastham@gmail.com>
2014-09-11 10:52:04 -0700

    New faculty category widget function.

--- library/widgets.php
+++ library/widgets.php
@@ -1,29 +1,96 @@
 <?php
-class coenv_base_subnav extends WP_Widget {
-          function coenv_base_subnav() {
-                    $widget_ops = array(
-                    'classname' => 'coenv_base_subnav',
-                    'description' => 'Second-level navigation'
+
+/*
+ * Faculty research areas
+ * In progress
+ */
+
+class coenv_base_fac_cats extends WP_Widget {
+
+     /**
+      * Register widget with WordPress.
+      */
+     function __construct() {
+          parent::__construct(
+               'coenv_base_fac_cats', // Base ID
+               __('Faculty category filter', 'text_domain'), // Name
+               array( 'description' => __( 'Allows filtering of faculty based on research area', 'text_domain' ), ) // Args
           );
+     }
 
-          $this->WP_Widget(
-                    'coenv_base_subnav',
-                    'Second-level navigation',
-                    $widget_ops
-          );
-}
-
-          function widget($args, $instance) { // widget sidebar output
-                    extract($args, EXTR_SKIP);
-                    echo $before_widget; // pre-widget code from theme
-
-                    echo 'This is a work in progress.';
-
-                    echo $after_widget; // post-widget code from theme
+     /**
+      * Front-end display of widget.
+      *
+      * @see WP_Widget::widget()
+      *
+      * @param array $args     Widget arguments.
+      * @param array $instance Saved values from database.
+      */
+     public function widget( $args, $instance ) {
+     
+          echo $args['before_widget'];
+          if ( ! empty( $instance['title'] ) ) {
+               echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
           }
-}
+          echo '<p>Sort faculty by research or focus area.</p>';
+                    $cats_args  = array(
+                      'orderby' => 'name',
+                      'order' => 'ASC',
+                      'taxonomy' => 'research_areas'
+                      );
+                    $cats = get_categories($cats_args);
+                    //if ($fac_cats) {
+                         echo '<ul class="fac-cats">';
+                         foreach($cats as $cat) { 
+                              echo '<li class="button"><a href="/faculty/?fac-cat=' . $cat->slug . '">' . $cat->name . '</a></li>';
+                         }
+                    //}
+          echo $args['after_widget'];
+     }
 
-add_action(
-          'widgets_init',
-          create_function('','return register_widget("coenv_base_subnav");')
-);
+     /**
+      * Back-end widget form.
+      *
+      * @see WP_Widget::form()
+      *
+      * @param array $instance Previously saved values from database.
+      */
+     public function form( $instance ) {
+          if ( isset( $instance[ 'title' ] ) ) {
+               $title = $instance[ 'title' ];
+          }
+          else {
+               $title = __( 'New title', 'text_domain' );
+          }
+          ?>
+          <p>
+          <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+          <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+          </p>
+          <?php 
+     }
+
+     /**
+      * Sanitize widget form values as they are saved.
+      *
+      * @see WP_Widget::update()
+      *
+      * @param array $new_instance Values just sent to be saved.
+      * @param array $old_instance Previously saved values from database.
+      *
+      * @return array Updated safe values to be saved.
+      */
+     public function update( $new_instance, $old_instance ) {
+          $instance = array();
+          $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+          return $instance;
+     }
+
+} // class coenv_base_fac_cats
+
+// register coenv_base_fac_cats widget
+function register_coenv_base_fac_cats() {
+    register_widget( 'coenv_base_fac_cats' );
+}
+add_action( 'widgets_init', 'register_coenv_base_fac_cats' );
