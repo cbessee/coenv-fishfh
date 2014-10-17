399db373499fec1ad78cdc48ce456b34cb5bf1b7
Drew Collins <drewcoll@uw.edu>
2014-10-17 16:51:38 -0700

    Publications single view with abstracts

---
+++ partials/partial-publications.php
@@ -0,0 +1,60 @@
+<?php  
+/**
+ * Publication content
+ */
+
+
+?>
+<article id="post-<?php the_ID() ?>" <?php post_class( 'article' ) ?>>
+
+	<header class="article__header">
+        <div class="article__meta">
+   		<?php if ( is_single() ) : ?>
+			<div class="share clearfix right" data-article-id="<?php the_ID(); ?>" data-article-title="<?php echo get_the_title(); ?>"
+			data-article-shortlink="<?php echo wp_get_shortlink(); ?>"
+			data-article-permalink="<?php echo the_permalink(); ?>"><a href="#"><i class="fi-share"></i>Share</a>
+            </div>
+        <?php endif ?>
+        </div>
+        <div class="faculty-title clearfix">
+			<h1 class="article__title left">
+			<?php if ( is_page() || is_single()) : ?>
+				<?php the_title() ?>
+			<?php else : ?>
+				<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a>
+			<?php endif ?>
+			</h1>
+		</div>
+		<div class="article__categories"><?php coenv_base_fac_terms($post->ID); ?></div>
+
+	</header>
+
+	<section class="article__content">
+		<?php
+		$publication_link = get_the_permalink();
+		$publication_citation = get_field('publication_citation');
+		$rows = get_field('publication_link');
+		echo '<div class="citation"><h3>Citation</h3>';
+		echo $publication_citation . '</div>';
+		echo '<div class="publication-links right">';
+		if($rows) {
+			foreach($rows as $row) {
+				if($row['publication_link_type'] == 'upload') {
+					echo '<a class="button" href="' . $row['publication_upload_file'] . '" target="_blank">Download PDF</a>';
+				} elseif ($row['publication_link_type'] == 'link') {
+					echo '<a class="button" href="' . $row['publication_link_url'] . '" target="_blank">Link to file</a>';
+				} 
+			}
+		}
+		echo '</div><hr />';
+		echo '<div class="abstract"><h3>Abstract</h3>';
+		echo get_field('publication_abstract');
+		echo '</div>';
+		?>
+	</section>
+    <?php
+    remove_filter( 'the_title', 'wptexturize' );
+    remove_filter( 'the_excerpt', 'wptexturize' );
+	?>
+
+</article><!-- .article -->
---
+++ single-publications.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+<div class="row">
+	<div class="columns large-12 section-title"><h1><a href="/research/publications">Publications</a></h1></div>
+	<div class="breadcrumbs"><!-- Breadcrumb NavXT 5.1.1 -->
+		<span typeof="v:Breadcrumb"><a rel="v:url" property="v:title" title="Go to School of Aquatic and Fishery Sciences." href="http://dev.fish.washington.edu" class="home">Home</a></span><a property="v:title" href="/research">Research</a><a property="v:title" href="/research/publications">Publications</a></span>
+	</div>
+	<div class="small-12 large-8 columns" role="main">
+	
+	<?php do_action('foundationPress_before_content'); ?>
+			<?php do_action('foundationPress_post_before_entry_content'); ?>
+			<div class="entry-content">
+			<?php if ( have_posts() ) : ?>
+
+					<?php while ( have_posts() ) : the_post() ?>
+
+						<?php get_template_part( 'partials/partial', 'publications' ) ?>
+
+					<?php endwhile ?>
+
+			<?php endif ?>
+			</div>
+			<footer>
+				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
+				<p><?php the_tags(); ?></p>
+			</footer>
+			<?php if ( is_active_sidebar( 'after-content' ) ) : ?>
+				<div id="after-content" class="after-content widget-area" role="complementary">
+					<?php dynamic_sidebar( 'after-content' ); ?>
+				</div><!-- #after-content -->
+			<?php endif; ?>
+		</article>	
+	<?php do_action('foundationPress_after_content'); ?>
+
+	</div>
+	<?php get_sidebar(); ?>
+</div>	
+<?php get_footer(); ?>
