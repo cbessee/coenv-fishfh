e2b93756dc6b24cf07ddd3e01b15641b76ca71db
joeeastham <jeastham@gmail.com>
2014-10-22 14:56:37 -0700

    Adding my most recent client side changes.

---
+++ sidebar.php.REMOTE.85004.php
@@ -0,0 +1,22 @@
+<!--
+	<?php //do_action('foundationPress_before_sidebar'); ?>
+	<?php //dynamic_sidebar("sidebar-widgets"); ?>
+	<?php //do_action('foundationPress_after_sidebar'); ?>
+</aside>-->
+<?php  
+/**
+ * The sidebar template
+ *
+ * Serves up sidebar widgets for individual top level pages
+ */
+?>
+<aside id="sidebar" class="small-12 large-4 columns">
+<?php dynamic_sidebar('sidebar-widgets'); ?>
+<?php
+$ancestor_id = coenv_base_get_ancestor('ID');
+if (!function_exists('dynamic_sidebar') || !dynamic_sidebar( $ancestor_id )):
+
+	dynamic_sidebar( $ancestor_id );
+endif;
+?>
+</aside>
