d61c2d7e47d6fb67f4c5a85e7ef646393d582e27
coenvweb <coenvweb@uw.edu>
2014-10-23 11:00:42 -0700

    Removing junk

--- sidebar.php.LOCAL.85004.php
+++
@@ -1,5 +0,0 @@
-<aside id="sidebar" class="small-12 medium-4 columns">
-	<?php do_action('foundationPress_before_sidebar'); ?>
-	<?php dynamic_sidebar("sidebar-widgets"); ?>
-	<?php do_action('foundationPress_after_sidebar'); ?>
-</aside>
