d47a7a4cbeed1bc13596695b0e747c1edbe4beb8
joeeastham <jeastham@gmail.com>
2014-10-25 15:55:52 -0700

    Removing pager function. Foundationpress already has a nice one. :)

--- templates/faculty.php
+++ templates/faculty.php
@@ -83,11 +83,13 @@ $wp_query = new WP_Query( $teach_research_args );
 		endwhile;
 		?>
 				<div class="pager">
-					<!--
-  <div class="alignleft"><?php //previous_posts_link('&laquo; Previous') ?></div>
-  <div class="alignright"><?php //next_posts_link('More &raquo;') ?></div>
--->
-				<?php coenv_base_num_pagination($wp_query->max_num_pages);?>
+					<?php /* Display navigation to next/previous pages when applicable */ ?>
+	<?php if ( function_exists('FoundationPress_pagination') ) { FoundationPress_pagination(); } else if ( is_paged() ) { ?>
+		<nav id="post-nav">
+			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'FoundationPress' ) ); ?></div>
+			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'FoundationPress' ) ); ?></div>
+		</nav>
+	<?php } ?>
   </div>
 		
 
