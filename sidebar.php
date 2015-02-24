a45b9c886cca1a96be4291a78b018e092232479a
joeeastham <jeastham@gmail.com>
2015-02-24 11:17:36 -0800

    more widget and subnav updates

--- sidebar.php
+++ sidebar.php
@@ -12,14 +12,14 @@
 ?>
 <aside id="sidebar" class="small-12 medium-4 large-3 columns">
 <?php
-if (!is_front_page()) {
+/*if (!is_front_page()) {
 	echo '<div class="coenv_base_subnav">';
 		if ($GLOBALS['post']->post_parent) {
 			echo coenv_base_section_title($GLOBALS['post']->ID);
 		}
 		echo coenv_base_hierarchical_submenu($GLOBALS['post']->ID);
 	echo '</div>';
-}
+}*/
 ?>
 <?php dynamic_sidebar('sidebar-widgets'); ?>
 <?php
