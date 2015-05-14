fd17f6d5eab20d6e2353ea7d010f3843c75d4f13
joeeastham <jeastham@gmail.com>
2015-05-14 12:16:36 -0700

    grunt compiled files

--- js/app.js
+++ js/app.js
@@ -269,13 +269,9 @@ e.data("index",e.data("index")+1),g.addClass("moved"),c.rtl?(f.css({right:-(100*
 
             // slick slider
             $('.homepage-features').slick({
-            	                autoplay: false,
-                autoplaySpeed: 3000,
-                dots: true,
-                draggable: false,
-                pauseOnDotsHover: true,
-                arrows: false,
-				  infinite: true,
+ 
+
+                  infinite: true,
                   speed: 500,
                   fade: true,
                   cssEase: 'linear'
@@ -344,26 +340,15 @@ $.fn.blogHeader = function () {
     } );
 };
 
-jQuery(function ($) {
+
+
+
+
+
+
+
+;jQuery(function ($) {
 	'use strict';
 	$( ".single-faculty li.page-item-35" ).removeClass( "active" );
 	$( ".single-faculty li.page-item-31" ).addClass( "active" );
-
-	$("ul.slick-dots").contents().appendTo('.my-slick-dots').end();
-
-	$(".feature-controls .slick-p").click(function(e) {
-		event.preventDefault();
-    	$(".homepage-features").slickPrev(); // Switched to '.slick-slider'
-	});
-
-	$(".feature-controls .slick-n").click(function(e) {
-		event.preventDefault();
-    	$(".homepage-features").slickNext(); // Switched to '.slick-slider'
-	});
-
-
-
-
-
-
 });
