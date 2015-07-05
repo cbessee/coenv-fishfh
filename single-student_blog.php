3ca215bd260f17ae4983e2d5bb13274bde92509b
joeeastham <jeastham@gmail.com>
2015-07-04 18:55:32 -0700

    More sidebar/content area column changes

--- single-student_blog.php
+++ single-student_blog.php
@@ -6,7 +6,7 @@ Template Name: Student Blog
 
 <?php get_header(); ?>
 <div class="row">
-	<div class="small-12 medium-8 columns right" role="main">
+	<div class="small-12 medium-9 columns right" role="main">
 		<div class="news clearfix">
 			<div class="share right" data-article-id="<?php the_ID(); ?>" data-article-title="<?php echo get_the_title(); ?>" data-article-shortlink="<?php echo wp_get_shortlink(); ?>" data-article-permalink="<?php echo the_permalink(); ?>"><a href="#"><i class="fi-share"></i>Share</a>
         </div>
