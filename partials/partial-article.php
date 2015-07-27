00aaa5223583189e6b274e84bb4fab0d6f62fca8
Joe Eastham <coenvweb@uw.edu>
2015-07-27 11:28:18 -0700

    Intranet: Draft intranet section header and remove meta from partial

--- partials/partial-article.php
+++ partials/partial-article.php
@@ -7,7 +7,7 @@
 
 	<header class="article__header">
         <div class="article__meta">
-            <?php if ( !is_page() ) : ?>
+            <?php if ( !is_page() && (get_post_type() != 'intranet_page') ) : ?>
 			<div class="post-info">
 				<time class="article__time" datetime="<?php echo get_the_date('Y-m-d h:i:s') ?>"><?php echo get_the_date('M j, Y') ?></time> 
 				<?php $categories = get_the_category_list(' ') ?>
