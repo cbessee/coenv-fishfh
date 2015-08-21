24ed2ae6f312f96315d9a4dcdc018ba240a0f035
Joe Eastham <coenvweb@uw.edu>
2015-08-21 11:11:00 -0700

    Attempting to fix image captions

--- functions.php
+++ functions.php
@@ -62,7 +62,8 @@ add_image_size( 'sm_sq', '120', '120', true );
 add_image_size( 'faculty_sm', '120', '120', true );
 
 add_image_size( 'news_small', '250', '188', true );
-add_image_size( 'news_medium', '350', '262', true );
+add_image_size( 'news_medium', '370', '276', true );
+add_image_size( 'news_large', '425', '317', true );
 
 
 
@@ -293,55 +294,3 @@ function coenv_url_ssl($url)
   return $url;
 }
 add_filter('wp_get_attachment_url', 'coenv_url_ssl');
-
-
-
-
-/**
-     * Gets all images attached to a post
-     * @return string
-     */
-    function wpse_get_images() {
-        global $post;
-        $id = intval( $post->ID );
-        $size = 'medium';
-        $attachments = get_children( array(
-                'post_parent' => $id,
-                'post_status' => 'inherit',
-                'post_type' => 'attachment',
-                'post_mime_type' => 'image',
-                'order' => 'ASC',
-                'orderby' => 'menu_order'
-            ) );
-        if ( empty( $attachments ) )
-                    return '';
-
-        $output = "\n";
-    /**
-     * Loop through each attachment
-     */
-    foreach ( $attachments as $id  => $attachment ) :
-
-        $title = esc_html( $attachment->post_title, 1 );
-        $img = wp_get_attachment_image_src( $id, $size );
-
-        $output .= '<a class="selector thumb" href="' . esc_url( wp_get_attachment_url( $id ) ) . '" title="' . esc_attr( $title ) . '">';
-        $output .= '<img class="aligncenter" src="' . esc_url( $img[0] ) . '" alt="' . esc_attr( $title ) . '" title="' . esc_attr( $title ) . '" />';
-        $output .= '</a>';
-
-    endforeach;
-
-        return $output;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
