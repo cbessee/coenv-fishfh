321554aebe8a76bdd32e5c0d1f8b2a14a6a5ec83
ross hytnen <rhytnen@gmail.com>
2017-05-10 09:55:58 -0700

    There is some scoping issue with the $ alias to jQuery that I think has something to do with JMigrate.
    Some plugsins (in particular wssleek.com's event calendar will not resolve $.fn or $(this) as a result but will resolve jQuery.fn and jQuery(this).
    
    This fixes the scoping issue at least for this case but it may not be the best overall solution.

--- js/app.js
+++ js/app.js
@@ -358,21 +358,21 @@ jQuery(function ($) {
 
 });
 
-$.fn.blogHeader = function () {
+jQuery.fn.blogHeader = function () {
     'use strict';
 
-    var $header = $(this),
+    var $header = jQuery(this),
             $selectCategory = $header.find('.select-category select'),
             $selectMonth = $header.find('.select-month select');
 
     $selectCategory.on( 'change', function () {
-        var term_id = $(this).val(),
-                url = $(this).parent('div').attr('data-url');
+        var term_id = jQuery(this).val(),
+                url = jQuery(this).parent('div').attr('data-url');
         window.location.href = url + term_id + '#filter';
     } );
 
     $selectMonth.on( 'change', function () {
-        var url = $(this).val();
+        var url = jQuery(this).val();
         window.location.href = url;
     } );
 };
